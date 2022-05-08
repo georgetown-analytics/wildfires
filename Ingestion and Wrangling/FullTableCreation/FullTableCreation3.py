24384a8257b6013b5538cf3e3c6420590bc8dd03
benjo1095 <benanderson1359@gmail.com>
2022-05-08 13:10:35 -0400

    Added additional code for more merging of FIRMS and WFIGS

--- Ingestion and Wrangling/FullTableCreation/FullTableCreation3.py
+++ Ingestion and Wrangling/FullTableCreation/FullTableCreation3.py
@@ -1,5 +1,5 @@
 """
-Code to merge several datasets to get a first full table to be tranformed for ML
+Code to merge several datasets to get full table(s) to be tranformed for ML
 
 Reads data from the teams S3 bucket and merges them all together
 
@@ -12,7 +12,7 @@ WFIGS Data ( Wildland Fire Interagency Geospatial Services) data dcan be found i
 https://data-nifc.opendata.arcgis.com/search?tags=Category%2Chistoric_wildlandfire_opendata 
 
 For team:
-    This version code was used to create MergeTable1 and MergeTable2 
+    This version code was used to create MergeTable3_FirmsAndBigWfigs2Round.csv and MergeTable4_FirmsAndBigWfigs1Round.csv
 
 BA_
 """
@@ -24,8 +24,7 @@ import geopy.distance
 import numpy as np
 
 """
-Pull and concat similar data to start.
-We will merge the different datatypes soon
+Pull from S3 and concat similar data to start.
 """
 
 #TODO For Team: enter the credentails below to run
@@ -57,23 +56,26 @@ df_viirs = pull_data(S3_Key_id, S3_Secret_key, file) #get VIIRS data
 file = 'USDAJan2018ToMar2022.csv'
 df_usda = pull_data(S3_Key_id, S3_Secret_key, file) #get USDA data 
 file = 'WFIGS_Pulled5-5-2022.csv'
-df_wfigs = pull_data(S3_Key_id, S3_Secret_key, file) #get WFIGS data 
+df_wfigs = pull_data(S3_Key_id, S3_Secret_key, file) #get WFIGS (small) data table 
+file = 'WFIGS_big_Pulled5-8-2022.csv'
+df_wfigsbig = pull_data(S3_Key_id, S3_Secret_key, file) #get WFIGS (big) data table 
 print('Data pulled from S3 into dataframes')
-
 #concatonate all of the FIRMS data 
 df_FIRMS = pd.concat([df_modis1, df_modis2, df_viirs])
 
 """
 All main data sets are retrieved in respective data frames
 
+In this file, lets start by merging FIRMS and WFIGS
+
 Start by converting FIRMS to a mergable dataframe
 """
 
+latlonground = 1 #Sets the number of decimal places to round latlong
 print('Adjusting the FIRMS dataframe for merging...')
 #get a rounded longitude and latitude 
-df_FIRMS['lat'] = round(df_FIRMS['latitude'], 1)
-df_FIRMS['long'] = round(df_FIRMS['longitude'], 1)
-
+df_FIRMS['lat'] = round(df_FIRMS['latitude'], latlonground) #rounds lat long into new column, using latlonground # of decimal pts
+df_FIRMS['long'] = round(df_FIRMS['longitude'], latlonground) #rounds lat long into new column, using latlonground # of decimal pts
 #convert the hour 
 def GetHour(acq_time):
     """
@@ -102,29 +104,25 @@ Now getting a similar key for the WFIGS database
 print('Adjusting the WFIGS dataframe for merging...')
 # now starting to transform the fire db
 #getting the fire discovered date into the same format as the same date
-df_wfigs['disc_date'] = df_wfigs['irwin_FireDiscoveryDateTime'].astype(str).str[0:10]
-df_wfigs['disc_date'] = df_wfigs['disc_date'].astype(str).str.replace('/', '-')
-#get the hour
-df_wfigs['hour'] = df_wfigs['irwin_FireDiscoveryDateTime'].astype(str).str[11:13] #make the column for just hour
-#convert the init long and lat frame to 2 decimals like i did in the satellite df 
-df_wfigs['init_lat_rounded'] = round(df_wfigs['irwin_InitialLatitude'], 1)
-df_wfigs['init_long_rounded'] = round(df_wfigs['irwin_InitialLongitude'], 1)
+df_wfigsbig['disc_date'] = df_wfigsbig['FireDiscoveryDateTime'].astype(str).str[0:10]
+df_wfigsbig['disc_date'] = df_wfigsbig['disc_date'].astype(str).str.replace('/', '-')
+# #convert the init long and lat frame to 2 decimals like i did in the satellite df 
+df_wfigsbig['init_lat_rounded'] = round(df_wfigsbig['Y'], latlonground) #rounds lat long into new column, using latlonground # of decimal pts
+df_wfigsbig['init_long_rounded'] = round(df_wfigsbig['X'], latlonground) #rounds lat long into new column, using latlonground # of decimal pts
 #make the date loc column
-df_wfigs['disc_date_loc'] = df_wfigs['disc_date'] + ',' + df_wfigs['init_lat_rounded'].astype(str) + ',' + df_wfigs['init_long_rounded'].astype(str)
-#make the column for date hour loc
-df_wfigs['disc_date_hour_loc'] = df_wfigs['disc_date'] + ',' + df_wfigs['hour'] + ',' + df_wfigs['init_lat_rounded'].astype(str) + ',' + df_wfigs['init_long_rounded'].astype(str)
+df_wfigsbig['disc_date_loc'] = df_wfigsbig['disc_date'] + ',' + df_wfigsbig['init_lat_rounded'].astype(str) + ',' + df_wfigsbig['init_long_rounded'].astype(str)
 
 """
-Both datframes should be ready for merging on the key
+Both dataframes should be ready for merging on the key
 
 Merging on the date + location key.
 """
 
 print('Merging the dataframes...')
-dfDiscovered = df_FIRMS.merge(df_wfigs, left_on='date_loc', right_on='disc_date_loc', how='inner') #creates df of only dsetected areas
-dfMerged = df_FIRMS.merge(df_wfigs, left_on='date_loc', right_on='disc_date_loc', how='left') #creates df of both 
+dfDiscovered = df_FIRMS.merge(df_wfigsbig, left_on='date_loc', right_on='disc_date_loc', how='inner') #creates df of only dsetected areas
+dfMerged = df_FIRMS.merge(df_wfigsbig, left_on='date_loc', right_on='disc_date_loc', how='left') #creates df of both 
 dfMerged['FIRE_DETECTED'] = dfMerged['OBJECTID'].isnull() #if there was no fire detectection set to true
-dfMerged['FIRE_DETECTED'] = ~dfMerged['FIRE_DETECTED'].astype(bool) #flip so that if detection set to true
+dfMerged['FIRE_DETECTED'] = ~dfMerged['FIRE_DETECTED'].astype(bool) #flip so that if detection merged set to true
 
 """
 Convert results to csv
